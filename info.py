90a91785bf2c4d94ddaf089b4df0f0d2fe6eef6a
S3HID N3W3B <166154843+NBOTZ001@users.noreply.github.com>
2025-08-28 14:25:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,12 +48,12 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "NBX")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'nawab_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY',True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'smallshorts.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'c632738c0c95a1750a15a120d6ac5f26a2ffb756')
-SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'smallshorts.com')
-SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', 'c632738c0c95a1750a15a120d6ac5f26a2ffb756')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
+VERIFY = bool(environ.get('VERIFY',False))
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'hypelinks.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'e0bd298d26f1219580b074e8bc13b825cbd6587b')
+SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'hypelinks.in')
+SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', 'e0bd298d26f1219580b074e8bc13b825cbd6587b')
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
